8a8348bf71551e502dd704c34f4bc85eb408f543
shathkumarA <shantkumar619@gmail.com>
2024-06-20 10:53:23 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def Rohith(request):
+    return HttpResponse("<h1><maruqee>Hit-man<maruqee><h1>")
+
+def Virat(request):
+    return HttpResponse("<h1><maruqee>Runs=Con-man<maruqee><h1>")
