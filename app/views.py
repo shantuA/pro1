e0b6e4cdc3818e8f9125c079c96c53e2ec353911
shantuA <shanthkumarandure>
2024-06-20 22:17:20 +0530

    added discription

--- app/views.py
+++ app/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def Rohith(request):
-    return HttpResponse("<h1><maruqee>Hit-man<maruqee><h1>")
+    return HttpResponse("<h1><center>Rohit Gurunath Sharma🏏<center><h1> <h6>Sharma is known for his timing, elegance, six-hitting abilities and leadership skills."
+                         " Sharma holds several batting records which famously include most sixes in international cricket,"
+                         "[a] most double centuries in ODI cricket (3), most centuries at Cricket World Cups (7) and"
+                          " joint most hundreds in Twenty20 Internationals (5)<h6>")
 
 def Virat(request):
-    return HttpResponse("<h1><maruqee>Runs=Con-man<maruqee><h1>")
+    return HttpResponse("<h1><center>Virat Kohli🏏<center><h1> <h6>Kohli is widely regarded as one of the greatest batsmen of all time.He holds the record as the"
+                         "highest run-scorer in IPL, ranks second in T20I, third in ODI, and stands as the fourth-highest in international cricket<h6>")
